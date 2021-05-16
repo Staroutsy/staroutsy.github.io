11b0c99049de025e916a21b69e9e17d29b561ed9
Starout <staroutsy@users.noreply.github.com>
2021-05-17 00:43:43 +0200

    .

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    function addElement () {
+    function displayImages () {
         var i;
         for (i = 0; i < 10; i++){
-            const newDiv = document.createElement("div");
-            newDiv.className = "image";
-            const newContent = document.createTextNode("UWUowoUWU");
-            newDiv.appendChild(newContent);
+            //const newDiv = document.createElement("div");
+            //newDiv.className = "image";
+            //const newContent = document.createTextNode("UWUowoUWU");
+            //newDiv.appendChild(newContent);
             
-            var currentDiv = document.getElementById("imageContent");
-            currentDiv.appendChild(newDiv);
+            //var currentDiv = document.getElementById("imageContent");
+            //currentDiv.appendChild(newDiv);
+
+            var img = document.createElement("img");
+            img.className = "image"
+            img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5PSVuJ7bzi34RvCYyTZTvuEd0eHD3Mt574g&usqp=CAU";
+            var src = document.getElementById("imageContent");
+            src.appendChild(img);
         };
     };
 
-    addElement();
+    displayImages();
 });
